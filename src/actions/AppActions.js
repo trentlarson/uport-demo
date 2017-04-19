eb7ca64bc5acfd5c6dc8d8950c6beead5156fc68
Jeff Scott Ward <jsward.17@gmail.com>
2017-04-19 00:15:42 -0400

    better states and actions

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,7 +1,82 @@
-// Actions
+// //////////////////////////////////////////////
+// Connect uPort
+// //////////////////////////////////////////////
+
 export const connectUport = (data) => {
   return {
     type: 'CONNECT_UPORT',
+    data
+  }
+}
+
+// //////////////////////////////////////////////
+// Open/Close Modal
+// //////////////////////////////////////////////
+
+export const openModal = () => {
+  return {
+    type: 'OPEN_MODAL'
+  }
+}
+
+export const closeModal = () => {
+  return {
+    type: 'CLOSE_MODAL'
+  }
+}
+
+// //////////////////////////////////////////////
+// Get Current Shares
+// //////////////////////////////////////////////
+
+export const getCurrentSharesREQUEST = () => {
+  return {
+    type: 'GET_CURRENT_SHARES_REQUEST'
+  }
+}
+export const getCurrentSharesSUCCESS = (data) => {
+  return {
+    type: 'GET_CURRENT_SHARES_SUCCESS',
+    data
+  }
+}
+export const getCurrentSharesERROR = (data) => {
+  return {
+    type: 'GET_CURRENT_SHARES_ERROR',
+    data
+  }
+}
+
+
+export const updatesharesInput = (data) => {
+  return {
+    type: 'UPDATE_SHARES_INPUT',
+    data
+  }
+}
+
+
+// //////////////////////////////////////////////
+// Buy Shares
+// //////////////////////////////////////////////
+
+export const buySharesREQUEST = (tx, amount) => {
+  return {
+    type: 'BUY_SHARES_REQUEST',
+    amount: amount,
+    buyingInProgress: true
+  }
+}
+export const buySharesSUCCESS = (tx, data) => {
+  return {
+    type: 'BUY_SHARES_SUCCESS',
+    tx: tx,
+    data
+  }
+}
+export const buySharesERROR = (data) => {
+  return {
+    type: 'BUY_SHARES_ERROR',
     data
   }
 }
--- src/reducers/AppReducer.js
+++ src/reducers/AppReducer.js
@@ -1,4 +1,6 @@
-let initialState = {}
+let initialState = {
+  sharesInput: 0 // Stupid FB warning about controlled inputs
+}
 
 export default(state = initialState, payload) => {
   switch (payload.type) {
@@ -6,6 +8,60 @@ export default(state = initialState, payload) => {
       return {
         ...state,
         uport: payload.data
+      }
+
+    case 'OPEN_MODAL':
+      return {
+        ...state,
+        modal: true
+      }
+    case 'CLOSE_MODAL':
+      return {
+        ...state,
+        modal: false
+      }
+
+    case 'GET_CURRENT_SHARES_REQUEST':
+      return {
+        ...state,
+        gettingShares: true
+      }
+    case 'GET_CURRENT_SHARES_SUCCESS':
+      return {
+        ...state,
+        gettingShares: false,
+        sharesTotal: payload.data
+      }
+    case 'GET_CURRENT_SHARES_ERROR':
+      return {
+        ...state,
+        gettingShares: false,
+        error: payload.data
+      }
+    case 'UPDATE_SHARES_INPUT':
+      return {
+        ...state,
+        sharesInput: payload.data
+      }
+    case 'BUY_SHARES_REQUEST':
+      return {
+        ...state,
+        buyingInProgress: true,
+        confirmingInProgress: true
+      }
+    case 'BUY_SHARES_SUCCESS':
+      return {
+        ...state,
+        txHash: payload.tx,
+        buyingInProgress: false,
+        confirmingInProgress: false,
+        sharesTotal: payload.data
+      }
+    case 'BUY_SHARES_ERROR':
+      return {
+        ...state,
+        buyingInProgress: false,
+        sharesTotal: payload.data
       }
     default:
       return state
