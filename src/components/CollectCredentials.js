2838ff5e1396677ddb937f09858ba4a9bc64c765
Zach Ferland <zachferland@gmail.com>
2018-05-09 20:52:21 -0400

    feat: experimental/test did and new lib usage

--- src/components/CollectCredentials.js
+++ src/components/CollectCredentials.js
@@ -45,6 +45,9 @@ const RELATIONSHIPCLAIM = 'User'
 const CERTIFICATECLAIM = 'uPort Demo'
 const Time30Days = () => Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60
 
+
+// TODO swap out with all
+
 class CollectCredentials extends Component {
 
   constructor (props) {
@@ -52,37 +55,39 @@ class CollectCredentials extends Component {
     this.credentialsbtnClickA = this.credentialsbtnClickA.bind(this)
     this.credentialsbtnClickB = this.credentialsbtnClickB.bind(this)
     this.credentialsbtnClickC = this.credentialsbtnClickC.bind(this)
-    console.log(this.props.uport.pushToken)
-    uportServer.push = transport.push.send(this.props.uport.pushToken, this.props.uport.publicEncKey, 'https://api.uport.space/pututu/sns')
+    console.log(this.props.uport.capabilities[0])
+    console.log(this.props.uport.publicEncKey)
+    uportServer.push = transport.push.send(this.props.uport.capabilities[0], this.props.uport.publicEncKey, 'https://api.uport.me/pututu/sns/')
   }
 
   credentialsbtnClickA () {
     uportServer.attest({
-      sub: this.props.uport.did,
-      claim: {name: this.props.uport.name},
-      exp: Time30Days()
+      sub: this.props.uport.iss,
+      claim: {name: this.props.uport.own.name},
+      exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000  // 30 days from now
     }).then(jwt => {
-      uportServer.push(`https://id.uport.me/req/${jwt}`, )
+      // uportConnect.request(`https://id.uport.me/add?attestations=${jwt}`)
+      uportServer.push(`https://id.uport.me/add?attestations=${jwt}`)
     })
   }
   credentialsbtnClickB () {
     uportServer.attest({
-      sub: this.props.uport.did,
+      sub: this.props.uport.iss,
       claim: {Relationship: RELATIONSHIPCLAIM},
-      exp: Time30Days()
+      exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000  // 30 days from now
     }).then(jwt => {
-      console.log(jwt)
-      uportConnect.request(jwt, 'credReqB')
-      // uportServer.push(`https://id.uport.me/req/${jwt}`, )
+      uportConnect.request(`https://id.uport.me/add?attestations=${jwt}`)
+      // uportServer.push(`https://id.uport.me/me?attest=${jwt}`)
     })
   }
   credentialsbtnClickC () {
     uportServer.attest({
-      sub: this.props.uport.did,
+      sub: this.props.uport.iss,
       claim: {Certificate: CERTIFICATECLAIM},
-      exp: Time30Days()
+      exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000  // 30 days from now
     }).then(jwt => {
-      uportServer.push(`https://id.uport.me/req/${jwt}`, )
+      uportConnect.request(`https://id.uport.me/add?attestations=${jwt}`)
+      // uportServer.push(`https://id.uport.me/me?attest=${jwt}`)
     })
   }
 
--- src/utilities/uportSetup.js
+++ src/utilities/uportSetup.js
@@ -1,40 +1,69 @@
 import Connect from 'uport-connect'
 import { Credentials } from 'uport'
-import { transport, message, crypto } from 'uport-core'
+import { transport, message } from 'uport-core'
 import { decodeJWT, verifyJWT } from 'did-jwt'
 import Web3 from 'web3'
 
-// TODO move to server
+// TODO could be helper func in uport-core, this is just from uport-js-client
+const getUrlParams = (url) => (
+  url.match(/[^&?]*?=[^&?]*/g)
+     .map((param) => param.split('='))
+     .reduce((params, param) => {
+       params[param[0]] = param[1]
+       return params
+     }, {}))
+
+
+// TODO this will move in connect with
+const connectTransport = (uri, {data}) => {
+  if (/requestToken/.test(uri)) {
+  const jwt = getUrlParams(uri)['requestToken']
+  let callback = null
+  if (jwt) callback = decodeJWT(jwt).payload.callback
+  if(/chasqui/.test(callback)) {
+    // TODO also with unified urls, the transport for chasqui has to look more like this
+    let isCancelled = false
+    const cancel = () => { isCancelled = true }
+    uri = message.util.paramsToQueryString(uri, {'type': 'post'})
+    transport.qr.send()(uri, { cancel })
+    return transport.chasqui.poll(callback, 2000, () => isCancelled).then(res => {
+      transport.qr.close()
+      return {res, data}
+    }, err => {
+      transport.qr.close()
+      throw new Error(err)
+    })
+  } else {
+    transport.qr.send()(uri)
+    // return closeQR ??
+    return Promise.resolve({data})
+  }
+} else {
+  console.log(uri)
+  console.log('calling chasqui send')
+  return  transport.qr.chasquiSend()(uri).then(res => ({res, data}))
+}
+}
+
 const credentials = new Credentials({
    address: '2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG',
    privateKey: 'c818c2665a8023102e430ef3b442f1915ed8dc3abcaffbc51c5394f03fc609e2'
 })
 
-const uportConnect = new Connect('DemoApp', {network: 'rinkeby'})
-// Because we are mocking server
-uportConnect.credentials = credentials
 
-uportConnect.verifyResponse = (token) => {
-      return verifyJWT(token, {audience: credentials.did}).then(res => {
-        uportConnect.doc = res.doc
-        return credentials.processDisclosurePayload(res)
-      })
-    }
-uportConnect.mobileTransport = transport.url.send()
-// Also because we are mocking server, using chasqui in place of our own server
-const CHASQUI_URL = 'https://chasqui.uport.me/api/v1/topic/'
-const uportServer = {
-  requestDisclosure: () => {
-    const reqObj = { requested: ['name', 'phone', 'country'],
-                     notifications: true,
-                     callbackUrl: uportConnect.isOnMobile ? window.location.href : CHASQUI_URL + crypto.randomString(16) }
-    return credentials.requestDisclosure(reqObj)
-  },
-  attest: credentials.attest.bind(credentials),
-  createVerificationRequest: (unsignedClaim, sub, aud) => {
-    const callbackUrl = uportConnect.isOnMobile ? window.location.href : CHASQUI_URL + crypto.randomString(16)
-    return credentials.createVerificationRequest(unsignedClaim, sub, callbackUrl, aud)
+// TODO need handle parsing a bit differently in connect
+const verifyResponse = (res) => {
+    console.log('verify')
+    const decodedToken = decodeJWT(res).payload
+    return verifyJWT(res, {audience: decodedToken.aud}).then(res => res.payload)
   }
+
+
+const uportConnect = new Connect('DemoApp', {network: 'rinkeby', transport: connectTransport, verifyResponse})
+
+const uportServer = {
+  requestDisclosure: credentials.requestDisclosure.bind(credentials),
+  attest: credentials.attest.bind(credentials)
 }
 
 const web3 = new Web3(uportConnect.getProvider())
