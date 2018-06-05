ba6febcaf4830cff4d7e0f4d65efd2b881b5e35f
Zach Ferland <zachferland@gmail.com>
2018-06-05 14:56:23 -0400

    feat: add latest connect release feat

--- src/utilities/uportSetup.js
+++ src/utilities/uportSetup.js
@@ -1,50 +1,51 @@
 import Connect from 'uport-connect'
 import { Credentials } from 'uport'
-import { transport, message } from 'uport-core'
+import { transport, message, crypto } from 'uport-core'
 import { decodeJWT, verifyJWT } from 'did-jwt'
 import Web3 from 'web3'
 
 // TODO could be helper func in uport-core, this is just from uport-js-client
-const getUrlParams = (url) => (
-  url.match(/[^&?]*?=[^&?]*/g)
-     .map((param) => param.split('='))
-     .reduce((params, param) => {
-       params[param[0]] = param[1]
-       return params
-     }, {}))
+// const getUrlParams = (url) => (
+//   url.match(/[^&?]*?=[^&?]*/g)
+//      .map((param) => param.split('='))
+//      .reduce((params, param) => {
+//        params[param[0]] = param[1]
+//        return params
+//      }, {}))
 
 
 // TODO this will move in connect with
-const connectTransport = (uri, {data}) => {
-  if (/requestToken/.test(uri)) {
-  const jwt = getUrlParams(uri)['requestToken']
-  let callback = null
-  if (jwt) callback = decodeJWT(jwt).payload.callback
-  if(/chasqui/.test(callback)) {
-    // TODO also with unified urls, the transport for chasqui has to look more like this
-    let isCancelled = false
-    const cancel = () => { isCancelled = true }
-    uri = message.util.paramsToQueryString(uri, {'type': 'post'})
-    transport.qr.send()(uri, { cancel })
-    return transport.chasqui.poll(callback, 2000, () => isCancelled).then(res => {
-      transport.qr.close()
-      return {res, data}
-    }, err => {
-      transport.qr.close()
-      throw new Error(err)
-    })
-  } else {
-    transport.qr.send()(uri)
-    // return closeQR ??
-    return Promise.resolve({data})
-  }
-} else {
-  console.log(uri)
-  console.log('calling chasqui send')
-  return  transport.qr.chasquiSend()(uri).then(res => ({res, data}))
-}
-}
+// const connectTransport = (uri, {data}) => {
+//   if (/requestToken/.test(uri)) {
+//   const jwt = getUrlParams(uri)['requestToken']
+//   let callback = null
+//   if (jwt) callback = decodeJWT(jwt).payload.callback
+//   if(/chasqui/.test(callback)) {
+//     // TODO also with unified urls, the transport for chasqui has to look more like this
+//     let isCancelled = false
+//     const cancel = () => { isCancelled = true }
+//     uri = message.util.paramsToQueryString(uri, {'type': 'post'})
+//     transport.qr.send()(uri, { cancel })
+//     return transport.chasqui.poll(callback, 2000, () => isCancelled).then(res => {
+//       transport.qr.close()
+//       return {res, data}
+//     }, err => {
+//       transport.qr.close()
+//       throw new Error(err)
+//     })
+//   } else {
+//     transport.qr.send()(uri)
+//     // return closeQR ??
+//     return Promise.resolve({data})
+//   }
+// } else {
+//   console.log(uri)
+//   console.log('calling chasqui sends')
+//   return  transport.qr.chasquiSend()(uri).then(res => ({res, data}))
+// }
+// }
 
+// TODO move to server
 const credentials = new Credentials({
    address: '2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG',
    privateKey: 'c818c2665a8023102e430ef3b442f1915ed8dc3abcaffbc51c5394f03fc609e2'
@@ -52,17 +53,33 @@ const credentials = new Credentials({
 
 
 // TODO need handle parsing a bit differently in connect
-const verifyResponse = (res) => {
-    console.log('verify')
-    const decodedToken = decodeJWT(res).payload
-    return verifyJWT(res, {audience: decodedToken.aud}).then(res => res.payload)
-  }
+// const verifyResponse = (res) => {
+//     console.log('verify')
+//     const decodedToken = decodeJWT(res).payload
+//     return verifyJWT(res, {audience: decodedToken.aud}).then(res => res.payload)
+//   }
 
 
-const uportConnect = new Connect('DemoApp', {network: 'rinkeby', transport: connectTransport, verifyResponse})
+const uportConnect = new Connect('DemoApp', {network: 'rinkeby'})
+// Because we are mocking server
+uportConnect.credentials = credentials
+// uportConnect.verifyResponse = credentials.verifyProfile.bind(credentials)
 
+uportConnect.verifyResponse = (token) => {
+      return verifyJWT(token, {audience: credentials.did}).then(res => {
+        uportConnect.doc = res.doc
+        return credentials.processDisclosurePayload(res)
+      })
+    }
+
+const CHASQUI_URL = 'https://chasqui.uport.me/api/v1/topic/'
 const uportServer = {
-  requestDisclosure: credentials.requestDisclosure.bind(credentials),
+  requestDisclosure: () => {
+    const reqObj = { requested: ['name', 'phone', 'country'],
+                     notifications: true,
+                     callbackUrl: CHASQUI_URL + crypto.randomString(16) }
+    return credentials.requestDisclosure(reqObj)
+  },
   attest: credentials.attest.bind(credentials)
 }
 
