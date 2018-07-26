18df94e256b2052f4b2f667c044199f59205a7a2
Zach Ferland <zachferland@gmail.com>
2018-07-26 09:06:41 -0400

    feat: use minor did release libs

--- src/components/SignTransaction.js
+++ src/components/SignTransaction.js
@@ -67,7 +67,10 @@ class SignTransaction extends Component {
   }
 
   getCurrentShares () {
-    getShares(this.props.uport.address, this.props.actions)
+    // TODO: Dump this check once MNID is default behavior
+    const addr = checkAddressMNID(this.props.uport.networkAddress)
+    const actions = this.props.actions
+    getShares(addr, actions)
   }
 
   buyShares (e) {
@@ -76,7 +79,7 @@ class SignTransaction extends Component {
     console.log('buyShares')
 
     let sharesNumber = this.props.sharesInput
-    const addr = checkAddressMNID(this.props.uport.nad)
+    const addr = checkAddressMNID(this.props.uport.networkAddress)
     const actions = this.props.actions
 
     console.log({sharesNumber, addr, actions})
