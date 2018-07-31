b1c83c6f24b14ce86ba4182e9ced7eb2f713daff
Zach Ferland <zachferland@gmail.com>
2018-07-31 14:41:34 -0400

    feat: update for most recent connect major release changes

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -87,7 +87,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -130,7 +130,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-        
+
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
@@ -165,7 +165,7 @@ module.exports = {
       }
     ]
   },
-  
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
@@ -214,19 +214,19 @@ module.exports = {
     // Try to dedupe duplicated modules, if any:
     new webpack.optimize.DedupePlugin(),
     // Minify the code.
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true, // React doesn't support IE8
-        warnings: false
-      },
-      mangle: {
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-        screw_ie8: true
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     screw_ie8: true, // React doesn't support IE8
+    //     warnings: false
+    //   },
+    //   mangle: {
+    //     screw_ie8: true
+    //   },
+    //   output: {
+    //     comments: false,
+    //     screw_ie8: true
+    //   }
+    // }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin('static/css/[name].[contenthash:8].css'),
     // Generate a manifest file which contains a mapping of all asset filenames
--- src/utilities/uportSetup.js
+++ src/utilities/uportSetup.js
@@ -1,44 +1,15 @@
 import Connect from 'uport-connect'
-import { Credentials } from 'uport'
-import { transport, message, crypto } from 'uport-core'
-import { decodeJWT, verifyJWT } from 'did-jwt'
 import Web3 from 'web3'
 
-// TODO move to server
-const credentials = new Credentials({
-   address: '2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG',
-   privateKey: 'c818c2665a8023102e430ef3b442f1915ed8dc3abcaffbc51c5394f03fc609e2'
-})
+// TODO move to server, if we want to use app identity in example, should setup server/client example
+// const credentials = new Credentials({
+//    address: '2oeXufHGDpU51bfKBsZDdu7Je9weJ3r7sVG',
+//    privateKey: 'c818c2665a8023102e430ef3b442f1915ed8dc3abcaffbc51c5394f03fc609e2'
+// })
 
-const uportConnect = new Connect('DemoApp', {network: 'rinkeby'})
-// Because we are mocking server
-uportConnect.credentials = credentials
-
-uportConnect.verifyResponse = (token) => {
-      return verifyJWT(token, {audience: credentials.did}).then(res => {
-        uportConnect.doc = res.doc
-        return credentials.processDisclosurePayload(res)
-      })
-    }
-uportConnect.mobileTransport = transport.url.send()
-// Also because we are mocking server, using chasqui in place of our own server
-const CHASQUI_URL = 'https://chasqui.uport.me/api/v1/topic/'
-const uportServer = {
-  requestDisclosure: () => {
-    const reqObj = { requested: ['name', 'phone', 'country'],
-                     notifications: true,
-                     callbackUrl: uportConnect.isOnMobile ? window.location.href : CHASQUI_URL + crypto.randomString(16) }
-    return credentials.requestDisclosure(reqObj)
-  },
-  attest: credentials.attest.bind(credentials),
-  createVerificationRequest: (unsignedClaim, sub, aud) => {
-    const callbackUrl = uportConnect.isOnMobile ? window.location.href : CHASQUI_URL + crypto.randomString(16)
-    return credentials.createVerificationRequest(unsignedClaim, sub, callbackUrl, aud)
-  }
-}
+const uportConnect = new Connect('DemoApp', {network: 'rinkeby', accountType: 'keypair'})
 
 const web3 = new Web3(uportConnect.getProvider())
 web3.eth.defaultAccount = '0xB42E70a3c6dd57003f4bFe7B06E370d21CDA8087'
 
-
-export { web3, uportConnect, uportServer }
+export { web3, uportConnect}
