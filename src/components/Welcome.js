5db2a46355c657d87d704b952dc25713dad10633
Zach Ferland <zachferland@gmail.com>
2018-08-08 14:16:00 -0400

    feat: add routes for better req/res handling, and load state on app load

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,8 +5,8 @@ import { bindActionCreators } from 'redux'
 import * as AppActions from '../actions/AppActions'
 import styled from 'styled-components'
 import { uportConnect } from '../utilities/uportSetup'
-// import { uportServer, uportConnect } from '../utilities/uportSetup'
-// import { crypto } from 'uport-core'
+import { withRouter, Link } from 'react-router-dom'
+
 
 const ConnectReqID = 'ConnectRequest'
 const WelcomeWrap = styled.section``
@@ -27,6 +27,7 @@ class Welcome extends Component {
       console.log(payload.res)
       const resObj = Object.assign(payload.res, {address: uportConnect.address, did: uportConnect.did, mnid: uportConnect.mnid})
       this.props.actions.connectUport(resObj)
+      this.props.history.push('/signclaim')
     })
   }
 
@@ -41,8 +42,7 @@ class Welcome extends Component {
       <WelcomeWrap>
         <h4>Build a Better dApp</h4>
         <SubText>Identity and transaction infrastructure for Ethereum</SubText>
-        <ConnectUport
-          onClick={this.connectUport}>
+        <ConnectUport  onClick={this.connectUport}>
           Connect with uPort
         </ConnectUport>
       </WelcomeWrap>
@@ -58,4 +58,5 @@ const mapStateToProps = (state, props) => {
 const mapDispatchToProps = (dispatch) => {
   return { actions: bindActionCreators(AppActions, dispatch) }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Welcome)
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Welcome))
