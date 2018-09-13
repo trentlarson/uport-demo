d7f17f3a18745b43df9a1a73f024b1e3499eb055
Zach Ferland <zachferland@gmail.com>
2018-09-12 21:46:47 -0400

    feat: update to latest lib changes (did-release)

--- src/utilities/uportSetup.js
+++ src/utilities/uportSetup.js
@@ -1,4 +1,4 @@
-import Connect from 'uport-connect'
+import { Connect } from 'uport-connect'
 import Web3 from 'web3'
 
 // TODO move to server, if we want to use app identity in example, should setup server/client example
@@ -7,9 +7,10 @@ import Web3 from 'web3'
 //    privateKey: 'c818c2665a8023102e430ef3b442f1915ed8dc3abcaffbc51c5394f03fc609e2'
 // })
 
-const uportConnect = new Connect('DemoApp', {network: 'rinkeby', accountType: 'keypair', usePush: true})
+const uportConnect = new Connect('DemoApp', {network: 'rinkeby', accountType: 'keypair'})
 
 const web3 = new Web3(uportConnect.getProvider())
 web3.eth.defaultAccount = '0xB42E70a3c6dd57003f4bFe7B06E370d21CDA8087'
+console.log(uportConnect.state)
 
 export { web3, uportConnect}
