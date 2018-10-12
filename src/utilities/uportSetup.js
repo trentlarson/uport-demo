d6e0b8f334c161cb742230e4d9d8a820e4d4f0ce
Rob Whitaker <rmw2@princeton.edu>
2018-10-12 13:58:42 -0400

    feat: add profileImage and bannerImage to connect instance

--- src/utilities/uportSetup.js
+++ src/utilities/uportSetup.js
@@ -7,7 +7,13 @@ import Web3 from 'web3'
 //    privateKey: 'c818c2665a8023102e430ef3b442f1915ed8dc3abcaffbc51c5394f03fc609e2'
 // })
 
-const uportConnect = new Connect('DemoApp', {network: 'rinkeby', accountType: 'keypair'})
+const uportConnect = new Connect('uPort Demo', {
+  network: 'rinkeby', 
+  accountType: 'keypair',
+  profileImage: {"/": "/ipfs/Qmez4bdFmxPknbAoGzHmpjpLjQFChq39h5UMPGiwUHgt8f"},
+  bannerImage: {"/": "/ipfs/QmUA7P9tTx3eQQi7k6VfGMADmDDyvG6xdn5P4yXxcMJs7a"},
+  description: "uPort demo sample description",
+})
 
 const web3 = new Web3(uportConnect.getProvider())
 web3.eth.defaultAccount = '0xB42E70a3c6dd57003f4bFe7B06E370d21CDA8087'
