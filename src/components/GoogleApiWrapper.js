dee5abaa9a7d44d6d391b4068d2b64d8ead3bd7c
Trent <trent@trentlarson.com>
2019-04-14 15:55:23 -0600

    fix to work with latest tenure

--- src/components/GoogleApiWrapper.js
+++ src/components/GoogleApiWrapper.js
@@ -22,19 +22,20 @@ export class MapContainer extends Component {
   mapClicked = (mapProps, map, e) => {
     let lat = e.latLng.lat(), lng = e.latLng.lng()
     this.setState({ markerPosition: { lat, lng  } })
-    fetch('http://' + process.env.REACT_APP_ENDORSER_CH_HOST_PORT + `/api/report/residenceClaimsAtPoint?lat=${lat}&lon=${lng}`, {
+    fetch('http://' + process.env.REACT_APP_ENDORSER_CH_HOST_PORT + `/api/report/tenureClaimsAtPoint?lat=${lat}&lon=${lng}`, {
       method: 'GET',
       headers: {
         "Content-Type": "application/json"
       }})
       .then(response => response.json())
-      .then(data =>
-            (data.length > 0)
-            ? (data.length === 1)
-              ? this.setState({ polygonPaths: polygonPathsFromString(data[0].polygon) })
-              : alert("Multiple found.  Only showing one.")
-            : this.setState({ polygonPaths: [] })
-           )
+      .then(data => {
+        if (data.length == 0) {
+          this.setState({ polygonPaths: [] })
+        } else {
+          this.setState({ polygonPaths: polygonPathsFromString(data[0].polygon) })
+          alert("Multiple found.  Only showing one.")
+        }
+      })
   }
 
   render() {
@@ -42,7 +43,7 @@ export class MapContainer extends Component {
       <div>
         <Map
         google={this.props.google}
-        style={{ width: '600', height: '450' }}
+        style={{ width: 600, height: 450 }}
         zoom={16}
         initialCenter={{ lat: 40.883944, lng: -111.884787 }}
         onClick={this.mapClicked}
