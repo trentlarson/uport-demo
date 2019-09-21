fa765c0085bb833ec1c960b8a6d55328255d1a8b
Trent <trent@trentlarson.com>
2019-09-20 23:25:02 -0600

    finalize voting page

--- src/components/bvc/VoteIgniteSpeakers.js
+++ src/components/bvc/VoteIgniteSpeakers.js
@@ -2,13 +2,16 @@
 import React from 'react'
 import JSONInput from 'react-json-editor-ajrm'
 import { connect } from 'react-redux'
+import { HashLoader } from 'react-spinners'
 import { withRouter } from 'react-router-dom'
 import { bindActionCreators } from 'redux'
 import R from 'ramda'
 import styled from 'styled-components'
+import { verifyJWT } from 'did-jwt'
 
 import ErrorHandlingComponent from '../ErrorHandlingComponent'
 import * as AppActions from '../../actions/AppActions'
+import { getUserToken } from '../../utilities/claimsTest'
 import { uportConnect } from '../../utilities/uportSetup'
 
 const WelcomeWrap = styled.section``
@@ -23,46 +26,98 @@ const CANDIDATES = [
   {
     "@context": "http://schema.org",
     "@type": "VoteAction",
-    "candidate": "Jay Butler",
+    "actionOption": "A Motorcycle, A Crash, and Discovering A Long Road to Forgiveness",
+    "candidate": "Trent Larson",
+    "object": {
+      "event": {
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
+      }
+    }
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "VoteAction",
+    "actionOption": "Alternative Medicine",
+    "candidate": "Craig St Clair",
+    "object": {
+      "event": {
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
+      }
+    }
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "VoteAction",
     "actionOption": "Astronomy for Non-Scientists",
+    "candidate": "Jay Butler",
     "object": {
       "event": {
-        "name": "Ignite Bountiful Night"},
-        "startTime": "2019-10-09T19:00:00.000-06:00"
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
       }
+    }
   },
   {
     "@context": "http://schema.org",
     "@type": "VoteAction",
-    "candidate": "Bruce Jeppsen",
+    "actionOption": "Emergency Medicine",
+    "candidate": "Jess St Clair",
+    "object": {
+      "event": {
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
+      }
+    }
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "VoteAction",
+    "actionOption": "Estate Planning",
+    "candidate": "Larry Mulcock",
+    "object": {
+      "event": {
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
+      }
+    }
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "VoteAction",
     "actionOption": "How to Listen to Your Intuition",
+    "candidate": "Bruce Jeppsen",
     "object": {
       "event": {
-        "name": "Ignite Bountiful Night"},
-        "startTime": "2019-10-09T19:00:00.000-06:00"
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
       }
+    }
   },
   {
     "@context": "http://schema.org",
     "@type": "VoteAction",
-    "candidate": "Judd Arnold",
     "actionOption": "Martial Arts",
+    "candidate": "Judd Arnold",
     "object": {
       "event": {
-        "name": "Ignite Bountiful Night"},
-        "startTime": "2019-10-09T19:00:00.000-06:00"
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
       }
+    }
   },
   {
     "@context": "http://schema.org",
     "@type": "VoteAction",
-    "candidate": "Brandon McKean",
-    "actionOption": "Building Confidence, Especially in Kids",
+    "actionOption": "Neurofeedback",
+    "candidate": "Larry Mulcock",
     "object": {
       "event": {
-        "name": "Ignite Bountiful Night"},
-        "startTime": "2019-10-09T19:00:00.000-06:00"
+        "name": "Ignite Bountiful Night",
+        "startDate": "2019-10-09T19:00:00.000-06:00"
       }
+    }
   }
 ]
 
@@ -71,10 +126,19 @@ class VoteIgniteSpeakers extends ErrorHandlingComponent {
   constructor (props) {
     super(props)
     this.state = {
-      votes: []
+      claimStoredResponse: '',
+      loading: false,
+      votes: [],
     }
     this.addRemoveVote = this.addRemoveVote.bind(this)
     this.signClaim = this.signClaim.bind(this)
+    this.handleSignedClaim = this.handleSignedClaim.bind(this)
+
+    uportConnect.onResponse('SignRequest')
+      .then(this.handleSignedClaim)
+      .catch(error => {
+        this.setState({responseJWT: error})
+      })
   }
 
   addRemoveVote (item) {
@@ -94,17 +158,51 @@ class VoteIgniteSpeakers extends ErrorHandlingComponent {
     uportConnect.requestVerificationSignature(claimToSign, uportConnect.did, 'SignRequest')
   }
 
+  handleSignedClaim(res) {
+    //console.log(res) // format: { id: "SignRequest", payload: "...", data: undefined }
+    this.setState({loading: true})
+
+    verifyJWT(res.payload).then(json => {
+      console.log("Response JWT", res.payload)
+      console.log("Response JSON", json.payload)
+      fetch('http://' + process.env.REACT_APP_ENDORSER_CH_HOST_PORT + '/api/claim', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Uport-Push-Token": getUserToken(this.props)
+        },
+        body: JSON.stringify({jwtEncoded:res.payload})})
+        .then(this.alertOrReturnJson("saving signed claim"))
+        .then(data => this.setState({ loading: false, claimStoredResponse: "Success!  Your claim is saved with ID " + data }))
+        .catch(err => this.setState({ loading: false }))
+    })
+    .catch(window.alert)
+
+  }
+
   render () {
     return (
       <WelcomeWrap>
 
         <h3>Vote for Ignite Bountiful Speakers</h3>
 
+        <div>
+        {/** The ClipLoader shows on the right without any CSS setting but the HashLoader doesn't. Weird. **/}
+        <HashLoader
+          color={'#FF0000'}
+          loading={this.state.loading}
+          size={30}
+          sizeUnit={"px"}
+        />
+        <span style={{'color':'#66FF00'}}>{this.state.claimStoredResponse}</span>
+        </div>
+
+
         <div style={{textAlign: 'left', marginRight: '20px'}}>
       {
         R.map(item =>
-          <div key={item.candidate + ' - "' + item.actionOption + '"'}>
-              <input type="checkbox" onClick={() => {this.addRemoveVote(item)}}/>{item.candidate} - "{item.actionOption}"<br/>
+          <div key={item.actionOption + ' by "' + item.candidate + '"'}>
+              <input type="checkbox" onClick={() => {this.addRemoveVote(item)}}/>"{item.actionOption}" by {item.candidate}<br/>
           </div>
         )(CANDIDATES)
       }
